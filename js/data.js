173364795cf66c00c3bf6c862bc06ba3c702f713
nicolelim <48461209+nicolelim0517@users.noreply.github.com>
2021-08-18 19:23:43 +0900

    시즌 버튼 추가, 검색 영역 추가

---
+++ js/data.js
@@ -0,0 +1,305 @@
+export default [{
+        id: 0,
+        title: "1화: 우리에겐 더 많은 여성들의 이야기가 필요해, <밥블레스유>",
+        category: "예능",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/1",
+        link: "http://program.tving.com/olive/bobblessyou?rcnt=1",
+    }, {
+        id: 1,
+        title: "2화: 여자가 다 하면 왜 안돼?, <미쓰백>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/2",
+        link: "https://www.netflix.com/title/81388039",
+        relatedworks: [{
+            title: "Gloria",
+            link: "https://movie.daum.net/moviedb/main?movieId=14529"
+        }],
+    }, {
+        id: 2,
+        title: "3화: 21세기에 로맨틱코미디를 만드는 법, <크레이지 리치 아시안>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/3",
+        link: "https://movie.daum.net/moviedb/main?movieId=117759",
+        relatedworks: [{
+            title: "내가 사랑했던 모든 남자들에게",
+            link: "https://www.netflix.com/title/81030842"
+        }, {
+            title: "상사에 대처하는 로맨틱한 자세",
+            link: "https://www.netflix.com/title/80184100"
+        }, {
+            title: "빅식",
+            link: "https://watcha.com/contents/share/mdKBJVV"
+        }],
+    }, {
+        id: 3,
+        title: "4화: 거울앞에서 너무 많은 시간을 보낸 우리들, <어느날 인생이 엉켰다>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/4",
+        link: "https://www.netflix.com/title/80189630",
+        relatedworks: [{
+            title: "친애하는 백인 여러분",
+            link: "https://www.netflix.com/title/80095698"
+        }, {
+            title: "거울 앞에서 너무 많은 시간을 보냈다 / 러네이 엥겔른",
+            link: "http://www.yes24.com/Product/Goods/54587899?OzSrank=1"
+        }]
+    }, {
+        id: 4,
+        title: "5화: 여성이 권력을 갖는다면, <하우스 오브 카드> 시즌 6",
+        category: "드라마",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/5",
+        link: "https://www.netflix.com/kr/title/70178217?source=35",
+        relatedworks: [{
+            title: "Veep",
+            link: "https://www.amazon.com/Veep-Season-1/dp/B00BS4L6YG"
+        }]
+    }, {
+        id: 5,
+        title: "6화: 춥고 지독한 서울에서 나로살기, <소공녀>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/6",
+        link: "https://www.netflix.com/title/81095657",
+        relatedworks: [{
+            title: "프란시스 하",
+            link: "https://movie.daum.net/moviedb/main?movieId=73620"
+        }]
+    }, {
+        id: 6,
+        title: "7화: 당신을 돌봐주러 왔어요, <툴리>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/7",
+        link: "https://www.netflix.com/kr/title/80216308",
+        relatedworks: [{
+            title: "바바둑",
+            link: "https://watcha.com/contents/share/mOPoo8g"
+        }, {
+            title: "아기낳는 만화",
+            link: "https://comic.naver.com/webtoon/list.nhn?titleId=703837&no=6&weekday=mon"
+        }]
+    }, {
+        id: 7,
+        title: "8화: 넷플릭스 시상식",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/8",
+        relatedworks: [{
+            title: "내가 사랑했던 모든 남자들에게",
+            link: "https://www.netflix.com/title/81030842"
+        }, {
+            title: "빨간 머리 앤",
+            link: "https://www.netflix.com/title/80136311"
+        }, {
+            title: "엘리 웡/성역은 없다",
+            link: "https://www.netflix.com/title/80186940"
+        }, {
+            title: "원데이 앳 어 타임",
+            link: "https://www.netflix.com/title/80095532"
+        }, {
+            title: "해나 개즈비/나의 이야기",
+            link: "https://www.netflix.com/title/80233611"
+        }, {
+            title: "하우스 오브 카드6",
+            link: "https://www.netflix.com/kr/title/70178217?source=35"
+        }, {
+            title: "크레이지 엑스 걸프렌드",
+            link: "https://www.netflix.com/title/80066227"
+        }, {
+            title: "그레이스",
+            link: "https://www.netflix.com/title/80119411"
+        }, {
+            title: "그레이스 앤 프랭키",
+            link: "https://www.netflix.com/title/80017537"
+        }, {
+            title: "닫힌 문을 열고",
+            link: "https://www.netflix.com/title/80216844"
+        }, {
+            title: "그랜마",
+            link: "https://watcha.com/ko-KR/contents/mO2MaMQ?external_link_referer=copytopaste&ref=share"
+        }]
+    }, {
+        id: 8,
+        title: "9화: 2018 영화와 여성",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/9",
+    }, {
+        id: 9,
+        title: "10화: 2018 드라마와 여성",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/10",
+    }, {
+        id: 10,
+        title: "11화: 2018 예능과 여성",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/11",
+    }, {
+        id: 11,
+        title: "12화: 우리는 모두 거짓말을 한다, <SKY 캐슬>",
+        category: "드라마",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/12",
+        link: "https://www.netflix.com/title/81030062",
+        relatedworks: [{
+            title: "아내의 자격",
+            link: "https://watcha.com/contents/share/txR49OP"
+        }]
+    }, {
+        id: 12,
+        title: "13화: 할 말이 있는 당신을 위한, <거리의 만찬>",
+        category: "시사교양",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/13",
+        link: "https://program.kbs.co.kr/1tv/culture/feastontheroad/pc/index.html",
+        relatedworks: [{
+            title: "어른이 되면",
+            link: "https://www.youtube.com/watch?v=z4eso1vAcAU"
+        }]
+    }, {
+        id: 13,
+        title: "14화: 우아하고 호쾌한 여자 레슬링, <당갈>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/14",
+        link: "https://www.netflix.com/title/80166185",
+        relatedworks: [{
+            title: "우아하고 호쾌한 여자 축구",
+            link: "http://www.yes24.com/Product/Goods/61494041?OzSrank=1"
+        }]
+    }, {
+        id: 14,
+        title: "15화: 500만원에 집을 사는 법, <개를 훔치는완벽한 방법>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/15",
+        link: "https://watcha.com/contents/share/mWLjVAN",
+        relatedworks: [{
+            title: "우리들",
+            link: "https://watcha.com/contents/share/mOo0P9a"
+        }]
+    }, {
+        id: 15,
+        title: "16화: 산드라 블록과 함께라면 재앙도 두렵지않아, <버드박스>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/16",
+        link: "https://www.netflix.com/title/80196789",
+        relatedworks: [{
+            title: "콰이어트 플레이스",
+            link: "https://movie.daum.net/moviedb/main?movieId=117142"
+        }, {
+            title: "해가 지는 곳으로",
+            link: "http://www.yes24.com/Product/Goods/43504366?OzSrank=1"
+        }]
+    }, {
+        id: 16,
+        title: "17화: 설맞이 최고의 가족영화, <유전>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/17",
+        link: "https://www.netflix.com/title/80238910",
+        relatedworks: [{
+            title: "스탠바이 웬디",
+            link: "https://www.youtube.com/watch?v=Q7_0XouukTA"
+        }]
+    }, {
+        id: 17,
+        title: "18화: 성교육이 필요한 모두에게, <오티스의 비밀 상담소>",
+        category: "드라마",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/18",
+        link: "https://www.netflix.com/title/80197526",
+        relatedworks: [{
+            title: "아무도 대답해주지 않은 질문들",
+            link: "http://www.yes24.com/Product/Goods/51195045?OzSrank=1"
+        }]
+    }, {
+        id: 18,
+        title: "19화: 에밀리 블런트의 <메리 포핀스 리턴즈>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/19",
+        link: "https://www.youtube.com/watch?v=Q4umry-UFiY",
+        relatedworks: [{
+            title: "세이빙 미스터 뱅크스",
+            link: "https://www.youtube.com/watch?v=LKijo6kvQtg"
+        }]
+    }, {
+        id: 19,
+        title: "20화: 엄마, 나를 좋아하긴 해? <레이디 버드>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/20",
+        link: "https://www.youtube.com/watch?v=I8YCeoUtZeg",
+        relatedworks: [{
+            title: "미스트리스 아메리카",
+            link: "https://www.youtube.com/watch?v=pvpgQW6_R3I"
+        }]
+    }, {
+        id: 20,
+        title: "21화: 여자들이 다 하는 정치와 치정, <더페이버릿: 여왕의 여자>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/21",
+        link: "https://www.youtube.com/watch?v=dt26-RuyOYU",
+        relatedworks: [{
+            title: "메리, 퀸 오브 스코틀랜드",
+            link: "https://www.youtube.com/watch?v=oevu6WuBkNI"
+        }]
+    }, {
+        id: 21,
+        title: "22화: 삶의 복잡함을 그대로 보는법, <원데이 앳 어 타임>",
+        category: "시트콤",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/22",
+        link: "https://www.netflix.com/title/80095532",
+        relatedworks: [{
+            title: "김씨네 편의점",
+            link: "https://www.netflix.com/title/80199128"
+        }, {
+            title: "프레쉬 오프 더 보트",
+            link: "https://watcha.com/contents/share/tEQ40Kl"
+        }]
+    }, {
+        id: 22,
+        title: "23화: 증명할 필요가 없는 <캡틴마블>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/23",
+        link: "https://www.youtube.com/watch?v=VXZmea13-V0",
+        relatedworks: [{
+            title: "원더우먼",
+            link: "https://movie.daum.net/moviedb/main?movieId=90524"
+        }]
+    }, {
+        id: 23,
+        title: "24화: 생리 이야기, <피리어드: 더패드 프로젝트> & <피의 연대기>",
+        category: "다큐멘터리",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/24",
+        link: "https://www.netflix.com/title/81074663",
+        relatedworks: [{
+            title: "우리의 새빨간 비밀",
+            link: "http://www.yes24.com/Product/Goods/70968767?OzSrank=1"
+        }]
+    }, {
+        id: 24,
+        title: "25화: 87세, 여성, 대법관 <루스 베이더 긴즈버그: 나는 반대한다>",
+        category: "다큐멘터리",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/25",
+        link: "https://www.youtube.com/watch?v=nBWoKxQArOM",
+        relatedworks: [{
+            title: "세상을 바꾼 변호인",
+            link: "https://www.youtube.com/watch?v=7fXVxq_9jxc"
+        }]
+    }, {
+        id: 25,
+        title: "26화: 여자는 여자를 돕는다, <빅리틀 라이즈>",
+        category: "드라마",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/26",
+        link: "https://watcha.com/contents/share/tlAw8YR",
+        relatedworks: [{
+            title: "나를 찾아줘",
+            link: "https://watcha.com/ko-KR/contents/share/mdMgAmm"
+        }]
+    }, {
+        id: 26,
+        title: "27화: 남자는 못하는 <스파이>",
+        category: "영화",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/27",
+        link: "https://www.netflix.com/kr/title/80027379",
+        relatedworks: [{
+            title: "미란다",
+            link: "https://watcha.com/contents/share/tRw554l"
+        }]
+    }, {
+        id: 27,
+        title: "28화: BTS (Behind the Sisterhood)",
+        podcast: "https://audioclip.naver.com/channels/1111/clips/28",
+        relatedworks: [{
+            title: "영혼의 노숙자",
+            link: "https://audioclip.naver.com/channels/1182"
+        }]
+    }
+]
+  
